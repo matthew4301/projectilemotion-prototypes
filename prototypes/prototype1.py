35eae79ae407b1e4a0dae5daf2f0d08f882c188c
matthew4301 <matthewstothard43@gmail.com>
2023-09-12 12:00:44 +0100

    quadratic works

--- prototypes/prototype1.py
+++ prototypes/prototype1.py
@@ -62,12 +62,13 @@ def ballmotion():
     window.blit(font.render(f"Vertical Velocity: {vvelocity}m/s", True, black, None), (2,40))
     window.blit(font.render(f"Horizonal Velocity: {hvelocity}m/s", True, black, None), (2,80))
     window.blit(font.render(f"Angle: {angle}°", True, black, None),(2,0))
-    window.blit(font.render(f"duration: {round(duration,2)}s", True, black, None),(2,200))
+    window.blit(font.render(f"Time: {round(duration,2)}s", True, black, None),(2,200))
     window.blit(font.render("1m", True, black, None),(150,ball_r.y+10))
 
 def mainloop():
     global angle,vvelocity,hvelocity,duration
     run = True
+    graphs = False
     i=0
     x = []
     y = []
@@ -77,20 +78,19 @@ def mainloop():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-                x2.reverse()
-                y2.reverse()
-                up = (y2[0]*-1)
-                right = (x2[0]*-1)
+                up = y[len(y)-1]*-1
+                right = x2[len(x2)-1]*-1
                 for i in range(len(x2)):
                     x2[i]+=right
-                for i in range(len(x)):
                     x[i]+=right
                 for i in range(len(y2)):
+                    y2[i]*=-1
                     y2[i]+=up
-                for i in range(len(y)):
                     y[i]+=up
-                #plt.plot((x2+x),(y2+y))
-                plt.plot(x,y)
+                x2.reverse()
+                y2.reverse()
+                plt.plot((x2+x),(y2+y))
+                #plt.plot(x,y)
                 plt.savefig(f'image{i}.png')
         window.fill(white)
         pygame.draw.rect(window,green,ground_r)
@@ -98,14 +98,18 @@ def mainloop():
         ballmotion()
         keys = pygame.key.get_pressed()
         if keys[pygame.K_SPACE]:
+            currenttime = time.time()
+            if time.time() < time.time()+duration or graphs == True:
+                graphs = True
+                print("pass")
+                pass
+            print("not pass")
             window.blit(font.render("graph", True, black, None),(300,200))
             r = math.sqrt(vvelocity**2+hvelocity**2)
             x.append(i)
             x2.append(-i)
             y.append(r * math.sin(angle) * x[i] - (0.5) * 9.81 * x[i]**2)
             y2.append(-(r * math.sin(angle) * x[i] - (0.5) * 9.81 * x[i]**2))           
-            #y.append((math.tan(angle))*x[i] - (4.905/(r**2)*(math.cos(angle)**2))*(x[i]**2))
-            #y2.append(-((math.tan(angle))*x2[i] - (4.905/(r**2)*(math.cos(angle)**2))*(x2[i]**2))*-1)
             i+=1
         pygame.draw.line(window,blue,pygame.Vector2(ball_r.x,ball_r.y+10),pygame.Vector2((hdistance*150),(ball_r.y+10)),3)
         pygame.draw.line(window,green,pygame.Vector2(ball_r.x,ball_r.y+10),pygame.Vector2((ball_r.x),((info.current_h-210)-vdistance*150)),3)
