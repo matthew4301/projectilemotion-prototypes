bc248638bbcf320d747ff7c661b808c01db05d22
matthew4301 <matthewstothard43@gmail.com>
2023-09-25 20:56:46 +0100

    did stuff

--- prototype1.py
+++ prototype1.py
@@ -13,7 +13,7 @@ clock = pygame.time.Clock()
 fps = 60
 distance = 0
 vdistance = 0
-white = (255, 255, 255)
+white = (255,255,255)
 black = (0, 0, 0)
 red = (255, 0, 0)
 green = (0, 100, 0)
@@ -21,6 +21,7 @@ blue = (0, 0, 255)
 font = pygame.font.SysFont("Comic Sans MS", 30, False)
 mousex = (width)/2
 mousey = (height)/2
+gravity = 9.81
 
 ground_r = pygame.Rect(0, height-100, width, 100)
 ball_r = pygame.Rect(5,ground_r.y-10,10,10)
@@ -28,11 +29,11 @@ ball_r = pygame.Rect(5,ground_r.y-10,10,10)
 def velocity():
     global hdistance,vdistance,vvelocity,hvelocity,angle
     try:
-        vvelocity = round(math.sqrt(2*9.81*vdistance),2) 
+        vvelocity = round(math.sqrt(2*gravity*vdistance),2) 
     except ValueError:
         vvelocity = 0
     try:
-        hvelocity = round(math.sqrt(2*9.81*hdistance),2)
+        hvelocity = round(math.sqrt(2*gravity*hdistance),2)
     except ValueError:
         hvelocity = 0  
     
@@ -47,22 +48,27 @@ def ballmotion():
         mousey-=5
     if keys[pygame.K_DOWN]:
         mousey+=5
-    pygame.draw.aaline(window,red,pygame.math.Vector2(ball_r.x+10,ball_r.y),pygame.math.Vector2(mousex,mousey),5)
-    leny = (mousey-ground_r.y+10)/(width/4)
-    lenx = (mousex-15)/(width/4)
+    pygame.draw.line(window,red,pygame.math.Vector2(ball_r.x+10,ball_r.y),pygame.math.Vector2(mousex,mousey),5)
+    leny = height-((mousey+ball_r.y)-380)
+    lenx = mousex-(ball_r.x+10)
     try:
-        angle = (round(math.degrees(math.atan(leny/lenx)),1))*-1 
+        angle = (round(math.degrees(math.atan(leny/lenx)),1))
     except ZeroDivisionError:
         angle = 90
-    hdistance = round((mousex)/(width/4),2)
-    vdistance = round(((height-210)-mousey)/(width/4),2)
+    hdistance = round((lenx)/(width/2),2)
+    vdistance = round((leny)/(width/2),2)
     velocity()
-    duration = math.sqrt(vvelocity**2+hvelocity**2)/9.81
+    magv = math.sqrt(vvelocity**2+hvelocity**2)
+    #duration = ((math.sqrt(vvelocity**2+hvelocity**2))*math.sin(angle)/gravity)*2
+    duration = magv/gravity
+    
+    if duration < 0:
+        duration*=-1
     window.blit(font.render(f"Vertical Velocity: {vvelocity}m/s", True, black, None), (2,40))
     window.blit(font.render(f"Horizonal Velocity: {hvelocity}m/s", True, black, None), (2,80))
     window.blit(font.render(f"Angle: {angle}°", True, black, None),(2,0))
     window.blit(font.render(f"Time: {round(duration,2)}s", True, black, None),(2,120))
-    window.blit(font.render("1m", True, black, None),((width/4),ball_r.y+10))
+    window.blit(font.render("1m", True, black, None),((width/2),ball_r.y+10))
 
 def mainloop():
     global angle,vvelocity,hvelocity,duration,lenx,leny
@@ -86,26 +92,26 @@ def mainloop():
         if keys[pygame.K_SPACE]:
             currenttime.append(time.time())
             try:
-                if currenttime[j] >= currenttime[0]+(duration*4):
+                if currenttime[j] >= currenttime[0]+(duration):
                     for i in range(len(x)):
-                        newx = (ball_r.x+x[i]*5)
+                        newx = ((ball_r.x+5)+x[i]*5)
                         newy = (ball_r.y-y[i]*5)
                         ball_r2 = pygame.Rect(newx,newy,5,5)
                         pygame.draw.rect(window,black,ball_r2)
                 else:
-                    window.blit(font.render("graph", True, black, None),((width/4),200))
+                    window.blit(font.render("graph", True, black, None),((width/2),200))
                     r = vvelocity**2+hvelocity**2
                     x.append(i)
                     k = leny/lenx
-                    #y.append((k*(x[i]**2))-(9.81*(x[i]**2))*((1+(k**2))/(2*(r**2))))
-                    y.append(x[i]*math.tan(leny/lenx)-(9.81*(x[i]**2))*((1+(math.tan(leny/lenx)**2))/(2*(r**2))))
+                    # negative values causing bad graph maybe?????1?1?!?
+                    y.append(x[i]*math.tan(leny/lenx)-(gravity*(x[i]**2))*((1+(math.tan(leny/lenx)**2))/(2*(r**2))))
                     i+=1
                 j+=1
             except IndexError:
                 currenttime = []
                 j=0
-        pygame.draw.line(window,blue,pygame.Vector2(ball_r.x,ball_r.y+10),pygame.Vector2((hdistance*(width/4)),(ball_r.y+10)),3)
-        pygame.draw.line(window,green,pygame.Vector2(ball_r.x,ball_r.y+10),pygame.Vector2((ball_r.x),((height-210)-vdistance*(width/4))),3)
+        pygame.draw.line(window,blue,pygame.Vector2(ball_r.x,ball_r.y+10),pygame.Vector2((hdistance*(width/2)),(ball_r.y+10)),3)
+        pygame.draw.line(window,green,pygame.Vector2(ball_r.x,ball_r.y+10),pygame.Vector2((ball_r.x),((height-210)-vdistance*(width/2))),3)
         pygame.display.update()
         clock.tick(fps)
     pygame.quit()
